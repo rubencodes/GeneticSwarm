5db3fab6f8e16abe3a2bc5b4228e86fcbc953c85
Ruben Martinez Jr <rmartin@bowdoin.edu>
2015-02-17 11:00:09 -0500

    Adding in-page viz

--- WebSwarm/app/controllers/welcome_controller.rb
+++ WebSwarm/app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 		if session[:user_id]
 			@user = User.find_by(id: session[:user_id])
 			@swarm = SwarmBehavior.get_next_swarm_behavior(session[:user_id]) if @user
+			@swarm_behaviors = SwarmBehavior.get_next_swarm_behavior(session[:user_id]).find_all_subbehaviors if @user
 		end
   end
 end
